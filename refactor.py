b6733764619c901752382588a74af5648a26fea7
AleVlaKon <kontikov@mail.ru>
2025-01-30 23:48:12 +0300

    Добавил из Я.Диска файл рефакторинга

---
+++ refactor.py
@@ -0,0 +1,38 @@
+
+import openpyxl as xl
+
+z = xl.
+
+
+def format_cell_value(cell: xl.cell.cell.Cell, nuli: str):
+    #Добавляет nuli, если cell целое число
+    if type(cell.value) == int:                      
+        return f'{cell.value},{nuli}'
+    else:
+        return str(cell.value).replace('.', ',')
+    
+
+def dob_n(number: int | float) -> str:
+    '''
+    Добавляет хвостовые нули в протяженности 
+    в зависимости от количества нулей после запятой,
+    чтобы было 0,000 формат км
+    '''
+    return f"{number:,.3f}".replace('.', ',')
+        
+
+# workbook = xl.load_workbook('Ведомость.xlsx', data_only=True)
+# sheet_names = [i for i in workbook.sheetnames if i not in ['Лист1', 'ИД', 'В обсл', 'аб1']]
+# print(type(workbook['ИД']['A1']))
+
+def test_format_cell_value():
+    pass
+
+
+def test_dob_n():
+    assert dob_n(1) == '1,000'
+    assert dob_n(1.2) == '1,200'
+    assert dob_n(1.25) == '1,250'
+    assert dob_n(1.256) == '1,256'
+
+test_dob_n()
