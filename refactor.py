a5af70e53945b84a4a53dd0f6175df73cf1194c1
AleVlaKon <kontikov@mail.ru>
2025-01-31 00:02:56 +0300

    Переназвал и переформатировал функции

--- refactor.py
+++ refactor.py
@@ -4,7 +4,7 @@ import openpyxl as xl
 
 
 
-def format_cell_value(cell_value: int | float, nuli: str) -> str:
+def format_int_value(cell_value: int | float, nuli: str) -> str:
     #Добавляет nuli, если cell целое число
     if isinstance(cell_value, int):                      
         return f'{cell_value},{nuli}'
@@ -12,20 +12,20 @@ def format_cell_value(cell_value: int | float, nuli: str) -> str:
         return str(cell_value).replace('.', ',')
     
 
-def dob_n(number: int | float) -> str:
+def format_float_value(number: int | float, decimal_places: int) -> str:
     '''
     Добавляет хвостовые нули в протяженности 
     в зависимости от количества нулей после запятой,
     чтобы было 0,000 формат км
     '''
-    return f"{number:,.3f}".replace('.', ',')
+    return f"{number:,.{decimal_places}f}".replace('.', ',')
         
 
 # workbook = xl.load_workbook('Ведомость.xlsx', data_only=True)
 # sheet_names = [i for i in workbook.sheetnames if i not in ['Лист1', 'ИД', 'В обсл', 'аб1']]
 # print(type(workbook['ИД']['A1']))
 
-def test_format_cell_value():
+def test_format_int_value():
     pass
 
 
